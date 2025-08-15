78975d819dd7356affbbe7c8e18722e866cafe7f
unstable-code <assa0620@gmail.com>
2025-08-15 13:19:32 +0900

    Fix mapping forKB_KEY symbol ("⇤") certain key events.

--- main.c
+++ main.c
@@ -113,29 +113,29 @@ static void render_to_cairo(cairo_t *cairo, struct wsk_state *state,
 			cairo_set_source_u32(cairo, state->specialfg);
 
 			switch(key->sym) {
-				case XKB_KEY_space:     name = "⎵"; break;
+				case XKB_KEY_space:                             name = "⎵"; break;
 				case XKB_KEY_Control_L: case XKB_KEY_Control_R: name = "^"; break;
 				case XKB_KEY_Super_L:   case XKB_KEY_Super_R:   name = "⌘"; break;
 				case XKB_KEY_Alt_L:     case XKB_KEY_Alt_R:     name = "⌥"; break;
 				case XKB_KEY_Shift_L:   case XKB_KEY_Shift_R:   name = "⇧"; break;
-				case XKB_KEY_Return:    name = "⏎"; break;
-				case XKB_KEY_BackSpace: name = "⌫"; break;
-				case XKB_KEY_Delete:    name = "⌦"; break;
-				case XKB_KEY_Escape:    name = "⎋"; break;
-				case XKB_KEY_Up:        name = "↑"; break;
-				case XKB_KEY_Down:      name = "↓"; break;
-				case XKB_KEY_Left:      name = "←"; break;
-				case XKB_KEY_Right:     name = "→"; break;
-				case XKB_KEY_Next:      name = "↡"; break;
-				case XKB_KEY_Prior:     name = "↟"; break;
-				case XKB_KEY_Print:     name = "⎙"; break;
-				case XKB_KEY_Menu:      name = "≡"; break;
-				case XKB_KEY_Tab:       name = "⇥"; break;
-				case XKB_KEY_BackTab:   name = "⇤"; break;
-				case XKB_KEY_Caps_Lock: name = "⇪"; break;
-				case XKB_KEY_Home:      name = "⇱"; break;
-				case XKB_KEY_End:       name = "⇲"; break;
-				default:                name = key->name;
+				case XKB_KEY_Return:                            name = "⏎"; break;
+				case XKB_KEY_BackSpace:                         name = "⌫"; break;
+				case XKB_KEY_Delete:                            name = "⌦"; break;
+				case XKB_KEY_Escape:                            name = "⎋"; break;
+				case XKB_KEY_Up:                                name = "↑"; break;
+				case XKB_KEY_Down:                              name = "↓"; break;
+				case XKB_KEY_Left:                              name = "←"; break;
+				case XKB_KEY_Right:                             name = "→"; break;
+				case XKB_KEY_Next:                              name = "↡"; break;
+				case XKB_KEY_Prior:                             name = "↟"; break;
+				case XKB_KEY_Print:                             name = "⎙"; break;
+				case XKB_KEY_Menu:                              name = "≡"; break;
+				case XKB_KEY_Tab:                               name = "⇥"; break;
+				case XKB_KEY_ISO_Left_Tab:                      name = "⇤"; break;
+				case XKB_KEY_Caps_Lock:                         name = "⇪"; break;
+				case XKB_KEY_Home:                              name = "⇱"; break;
+				case XKB_KEY_End:                               name = "⇲"; break;
+				default:                                        name = key->name;
 			}
 		} else {
 			cairo_set_source_u32(cairo, state->foreground);
